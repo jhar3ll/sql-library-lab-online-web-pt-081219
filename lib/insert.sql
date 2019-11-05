99826a9a53959de3078d7514d46c49f6141cd9a1
Jharell Chambers <jharell.chambers@selu.edu>
2019-11-05 00:25:38 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,6 @@
-INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, "Borderlands", , )
+INSERT INTO series (id, title, author_id, subgenre_id) VALUES 
+(1, "Borderlands", 1, 1),
+(2, "Call of Duty",2, 2)
+
+IN
+
