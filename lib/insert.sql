e8d9e837d9b8d017772efc2d90faa9599bea6911
Jharell Chambers <jharell.chambers@selu.edu>
2019-11-05 01:28:54 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -19,7 +19,7 @@ INSERT INTO books (id, title, year, series_id) VALUES
 (6, "Modern Warfare 3", 2012, 2)
 
 INSERT INTO characters (id, name, motto, species, author_id) VALUES 
-(1, "")
+(1, "Claptrap", )
 
 
 
