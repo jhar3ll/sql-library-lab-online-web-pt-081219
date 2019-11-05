c5ad637d12f41d64e616337c144831d2f4be9ca4
Jharell Chambers <jharell.chambers@selu.edu>
2019-11-05 01:55:13 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -6,31 +6,32 @@ CREATE TABLE series (
 );
 
 CREATE TABLE subgenres (
-id INTEGER PRIMARY KEY,
-name TEXT
+  id INTEGER PRIMARY KEY,
+  name TEXT
 );
 
 CREATE TABLE authors (
-id INTEGER PRIMARY KEY,
-name TEXT
+  id INTEGER PRIMARY KEY,
+  name TEXT
 );
 
 CREATE TABLE books (
-id INTEGER PRIMARY KEY,
-title TEXT,
-series_id INTEGER
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  year INTEGER,
+  series_id INTEGER
 );
 
-CREATE TABLE characters
-id INTEGER PRIMARY KEY,
-name TEXT,
-motto TEXT,
-species TEXT,
-author_id INTEGER
-); 
+CREATE TABLE character_books (
+  id INTEGER PRIMARY KEY,
+  character_id INTEGER,
+  book_id INTEGER
+);
 
-CREATE TABLE character_books(
-id INTEGER PRIMARY KEY,
-character_id INTEGER,
-book_id INTEGER
+CREATE TABLE characters (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  species TEXT,
+  motto TEXT,
+  author_id INTEGER
 );
