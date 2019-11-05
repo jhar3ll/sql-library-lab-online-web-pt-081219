118c566d2d86ffbaf19ec24a4b02f13558b38f0a
Jharell Chambers <jharell.chambers@selu.edu>
2019-11-05 01:35:37 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -22,7 +22,8 @@ INSERT INTO characters (id, name, motto, species, author_id) VALUES
 (1, "Claptrap", "Let me teach you the ways of magic", "robot", 1)
 (2, "Handsome Jack", "Hey, kiddo. Jack here - president of Hyperion", "siren" 1)
 (3, "Lilith", "More loot? Don't mind if I do.", "siren", 1)
-(4, "Makarov, ")
-(5, "Captain Price", "Just another day at the office.", "human", 2)
-(6, "Soap", "Break's over. Let's go", )
+
+(4, "Makarov", "Those who would trade their freedom for security deserve neither.", "villian", 2)
+(5, "Captain Price", "Just another day at the office.", "hero", 2)
+(6, "Soap", "Break's over. Let's go", "hero", 2)
 
