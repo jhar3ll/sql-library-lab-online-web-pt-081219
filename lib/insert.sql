a7b05299e40f909c0aba04145eab44a164416cca
Jharell Chambers <jharell.chambers@selu.edu>
2019-11-05 00:43:48 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,5 +2,17 @@ INSERT INTO series (id, title, author_id, subgenre_id) VALUES
 (1, "Borderlands", 1, 1),
 (2, "Call of Duty",2, 2)
 
-INSERT INTO sub
+INSERT INTO subgenres (id, name) VALUES 
+(1, "RPG")
+(2, "FPS")
+
+INSERT INTO authors (id, name) VALUES
+(1, "2K Games")
+(2, "Activision")
+
+INSERT INTO books (id, title, year, series_id) VALUES
+(1, "Borderlands 1", "2009", 1)
+(2, "Borderlands 2", "2010", 1)
+(3, "Borderlands 3", "2019", 1)
+(4, "Black Ops", "2010")
 
